fd51b3a673f311f4a0607314faecfeae4ebc5598
wajid90 <wajid90273@gmail.com>
2023-09-03 07:40:25 -0400

    update product bug filxed

--- controllers/product.js
+++ controllers/product.js
@@ -76,7 +76,7 @@ export const createProduct = asyncError(async (req, res, next) => {
 });
 
 export const updateProduct = asyncError(async (req, res, next) => {
-  const { name, description, category, price, stock } = req.body;
+  const { name, description, category, price, stock,feature } = req.body;
 
   const product = await Product.findById(req.params.id);
   if (!product) return next(new ErrorHandler("Product not found", 404));
@@ -86,6 +86,7 @@ export const updateProduct = asyncError(async (req, res, next) => {
   if (category) product.category = category;
   if (price) product.price = price;
   if (stock) product.stock = stock;
+  if (feature) product.feature = feature;
 
   await product.save();
 
